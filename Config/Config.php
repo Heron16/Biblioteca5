bd2d34423073f44412062cb18685d503206ea017
unknown <Administrador@GI-COL-TADS-60.integrado.int>
2025-09-10 20:29:45 -0300

    Adiciona pasta Config

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/SISTEMA%20BIBLO%20DE%20BIBLIOTECAS%20MVC%20PHP%208.1/biblio/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "biblioteca";
+const charset = "charset=utf8";
+?>
